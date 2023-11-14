3ed3dee0c42b1e67364787e4c6982f0a203654f1
Vikram meena <Vikrammeena1142@gmail.com>
2023-11-15 01:27:21 +0530

    add loggers and connect to the db

---
+++ config/ConnDB.js
@@ -0,0 +1,7 @@
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+  });
+  module.exports = con
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const con = require('./config/ConnDB')
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(logger)
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
 
@@ -18,4 +32,9 @@ app.all('*', (req, res) => {
     }
 })
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)) 
+app.use(errorHandler)
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("DB Connected!");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  });
